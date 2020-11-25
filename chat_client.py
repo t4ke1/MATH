29a6bd23341bf714fd1f89150cf6fde1e34b7235
Wiskey666 <47121348+Wiskey666@users.noreply.github.com>
2020-11-25 21:35:02 +0200

    Add files via upload

---
+++ chat_client.py
@@ -0,0 +1,77 @@
+import sys
+import socket
+import select
+import errno
+from colorama import init, Fore, Back, Style
+import socket
+import threading
+
+init(convert=True)
+HEADER_LENGTH = 10
+
+print("\033[36m" + """
+    ███╗   ███╗ █████╗ ████████╗██╗  ██╗
+    ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
+    ██╔████╔██║███████║   ██║   ███████║
+    ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
+    ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
+    ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
+
+    -----------------------------------
+    Version: Alpha 1_0.2
+    Author: Wiskey666
+    Mail: 1488step@horsefucker.org
+    DS: yourmomgay#1488
+    -----------------------------------
+
+    This is a console chat for special lovers of this shit.
+    The chat will be improved (both the client side and the server side).
+    Thank you for noticing and trying it!
+
+    -----------------------------------
+""")
+print(Style.RESET_ALL)
+
+IP = input('IP: ')
+PORT = int(input('Port: '))
+
+# Choosing Nickname
+nickname = input("Username: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((IP, PORT))
+
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print("\033[33m" + message)
+                print(Style.RESET_ALL)
+        except:
+            # Close Connection When Error
+            print("[-] An error occured!")
+            client.close()
+            break
+
+# Sending Messages To Server
+def write():
+    while True:
+        try:
+            message = '<{}> {}'.format(nickname, input(''))
+            client.send(message.encode('ascii'))
+        except:
+            pass
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
