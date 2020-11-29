677e045cba3b07f1757706b1263400d2e6b88604
Wiskey666 <47121348+Wiskey666@users.noreply.github.com>
2020-11-29 18:14:25 +0200

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -1,3 +1,4 @@
+# Imports
 import sys
 import socket
 import select
@@ -6,74 +7,145 @@ from colorama import init, Fore, Back, Style
 import socket
 import threading
 import time
+import os
 
 from win10toast import ToastNotifier
 
+# Inits
 toaster = ToastNotifier()
-
 init(convert=True)
 
-print("\033[36m" + """
-    ███╗   ███╗ █████╗ ████████╗██╗  ██╗
-    ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
-    ██╔████╔██║███████║   ██║   ███████║
-    ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
-    ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
-    ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
-    -----------------------------------
-    Version: Alpha 1_0.6.4
+print("\33[37m\33[1m","""
+                   :::!~!!!!!:.
+                  .xUHWH!! !!?M88WHX:.
+                .X*#M@$!!  !X!M$$$$$$WWx:.
+               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:  
+             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!  
+             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!                      
+               !:~~~ .:!M"T#$$$$WX??#MRRMMM!               
+               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+             :X- M$$$$       `"T#$T~!8$WUXU~
+            :%`  ~#$$$m:        ~!~ ?$$$$$$
+          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+.....   -~~:<` !    ~?T#$$@@W@*?$$      /`
+W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+$R@i.~~ !     :   ~$$$$$B$$en:``
+?MXT@Wx.~    :     ~"##*$$$$M~
+    """ + Fore.WHITE)
+
+time.sleep(2)
+os.system('cls||clear')
+
+# Info
+print(Fore.CYAN + """
+     ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓ ██░ ██ 
+    ▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒▓██░ ██▒
+    ▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░▒██▀▀██░
+    ▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ ░▓█ ░██ 
+    ▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ ░▓█▒░██▓
+    ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░    ▒ ░░▒░▒
+    ░  ░      ░  ▒   ▒▒ ░   ░     ▒ ░▒░ ░
+    ░      ░     ░   ▒    ░       ░  ░░ ░
+       ░         ░  ░         ░  ░  ░                                     
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    Version: Alpha 1_1
     Author: Wiskey666
     Mail: 1488step@horsefucker.org
-    DS: yourmomgay#1488
-    -----------------------------------
+    DS: yourmomgay#1488""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+
     This is a console chat for special lovers of this shit.
     The chat will be improved (both the client side and the server side).
     Thank you for noticing and trying it!
-    -----------------------------------
-        IP     Port        Name
-    aoa.pp.ua  55555  #general_server
-    -----------------------------------
-""")
-print(Style.RESET_ALL)
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    """+ Fore.WHITE)
 
+# Client Settings
 IP = input('    IP: ')
-PORT = int(input('    Port: '))
-ver = "Alpha 1_0.6.4"
+if IP == '':
+    print(Fore.YELLOW + "\n    [!] Please enter IP!")
+    time.sleep(5)
+    sys.exit()
+    
+try:
+    PORT = int(input('    Port: '))
+    if PORT == '':
+        print(Fore.YELLOW + "\n    [!] Please enter port!")
+        time.sleep(5)
+        sys.exit()   
+except ValueError:
+    print(Fore.YELLOW + "\n    [!] Port must be an integer!")
+    time.sleep(5)
+    sys.exit()
+    
+ver = "Alpha 1_1"
 
 # Choosing Nickname
 nickname = input('    Username: ')
+if nickname == '':
+    print(Fore.YELLOW + "\n    [!] Please enter nickname!")
+    time.sleep(5)
+    sys.exit()
 
 print("""
     -----------------------------------    
 """)
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
+try:
+    client.connect((IP, PORT))
+except socket.error as e:
+    if e.errno == errno.ECONNREFUSED:
+        print(Fore.YELLOW + "\n    [!] Server refused connection or unavailable")
+    else:
+        raise
 
 # Listening to Server and Sending Nickname
 def receive():
     while True:
         try:
             # Receive Message From Server
-            # If 'NICK' Send Nickname
             message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
-                client.send(ver.encode('utf-8'))
+            
+            #Version Check
+            #If Server Sends Conflict Code - Kick
+            if "Code 409" in message:
+                print(Fore.RED +'░' + Fore.WHITE + '░' + Fore.RED + '░ ' + Fore.WHITE + message)
+                client.close()
+                break
+            if "Code 401" in message:
+                print(Fore.RED +'░' + Fore.WHITE + '░' + Fore.RED + '░ ' + Fore.WHITE + message)
+                client.close()
+                break
             else:
-                print(Fore.GREEN +'░' + Fore.YELLOW + '░' + Fore.GREEN + '░ ' + Fore.WHITE + message)
-                #print(Style.RESET_ALL)
+                # If 'NICK' Send Nickname
+                if message == 'NICK':
+                    client.send(nickname.encode('utf-8'))
+                    client.send(ver.encode('utf-8'))
+                else:
+                    print(Fore.RED +'░' + Fore.WHITE + '░' + Fore.RED + '░ ' + Fore.WHITE + message)
+                    #Check For New Messages
+                    if "<"+ nickname +">" in message or "[+]" in message or "[-]" in message or "[!]" in message:
+                        continue
+                    else:
+                       toaster.show_toast("MATH", message, threaded=True)
         except:
             # Close Connection When Error 
-            print(Fore.GREEN +'>' + Fore.YELLOW + '>' + Fore.GREEN + '> ' + Fore.RED + "[-] An error occured!")
+            print(Fore.RED +'>' + Fore.WHITE + '>' + Fore.RED + '> ' + Fore.RED + "[-] An error occured!")
             client.close()
-            break
-        
 # Sending Messages To Server
 def write():
     while True:
         try:
             message_text = input('')
+            # Check for blank massage
             if message_text == '':
                 continue
             else:
@@ -88,3 +160,4 @@ receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
